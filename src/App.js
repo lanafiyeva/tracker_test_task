19f2c8736d548484957ac881cb1d625d03e4f9c2
Lenara Anafieva <lenarik.ak@gmail.com>
2021-03-18 14:51:02 +0200

    initial mockup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Layout } from 'antd'
+import { Input, Space } from 'antd'
+import { Row, Col } from 'antd'
+
+const { Header, Footer, Content } = Layout
+const { Search } = Input
+const onSearch = (value) => console.log(value)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header className="App-header">
+          <h1>tracker</h1>
+        </Header>
+        <Content>
+          <Row>
+            <Col span={24}>
+              <Search
+                className="timer-input-container"
+                placeholder="input search text"
+                onSearch={onSearch}
+                enterButton
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <p className="timer-name">timer name</p>
+              <div className="timer-controls-container">
+                <div className="timer-control">Value</div>
+                <div className="timer-control">Pause</div>
+                <div className="timer-control">Remove</div>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24}>timer name</Col>
+          </Row>
+          <Row>
+            <Col span={24}>timer name</Col>
+          </Row>
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
