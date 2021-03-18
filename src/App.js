e0a5e4e9c1b774ab3be9598b5e7e764f8687021e
Lenara Anafieva <lenarik.ak@gmail.com>
2021-03-18 18:52:14 +0200

    css changes

--- src/App.js
+++ src/App.js
@@ -16,22 +16,26 @@ function App() {
         </Header>
         <Content>
           <Row>
-            <Col span={24}>
-              <Search
-                className="timer-input-container"
-                placeholder="input search text"
-                onSearch={onSearch}
-                enterButton
-              />
+            <Col className="input-container" span={24}>
+              <input className="input-field"></input>
+              <div className="input-button">
+                <div class="logo">
+                  <img src="/round_play_circle_filled_black_18dp.png"></img>
+                </div>
+              </div>
             </Col>
           </Row>
           <Row>
-            <Col span={24}>
+            <Col className="record-container" span={24}>
               <p className="timer-name">timer name</p>
               <div className="timer-controls-container">
-                <div className="timer-control">Value</div>
-                <div className="timer-control">Pause</div>
-                <div className="timer-control">Remove</div>
+                <div className="timer-control">00:05:45</div>
+                <div className="timer-control">
+                  <img src="/round_pause_circle_outline_black_18dp.png"></img>
+                </div>
+                <div className="timer-control">
+                  <img src="/round_remove_circle_outline_black_18dp.png"></img>
+                </div>
               </div>
             </Col>
           </Row>
